603f2a4402a7248f5ebfeb35174ca4aabbf9d020
Biswanath Nanda <nanda.biswanath@outlook.com>
2026-07-26 01:47:23 +0530

    Fix SKU capacity issue by updating main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_windows_virtual_machine" "windows_vm" {
   name                  = "${var.prefix}-vm"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B2s"
+  size                  = "Standard_B2ms"
   admin_username        = "adminuser"
   admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.nic.id]
